3547ab2d7e31548e9a91c242626e76f45089f7e3
Matthew Martz <xiao_haozi@mutaku.com>
2012-03-21 16:12:07 -0400

    setup django tidbits

---
+++ sounds/admin.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf_8 -*-
+
+from sounddrop.models import *
+from django.contrib import admin
+import hashlib
+from django.utils.encoding import smart_str
---
+++ sounds/models.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+#
+# -*- coding: utf_8 -*-
+
+from django.db import models
+

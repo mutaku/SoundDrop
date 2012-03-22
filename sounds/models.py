ece7df8c79300168c4df8c7665628632fa29597c
Matthew Martz <xiao_haozi@mutaku.com>
2012-03-22 00:21:26 -0400

    moved the clip model form to admin since that's where we'll be harnessing it...

--- sounds/models.py
+++ sounds/models.py
@@ -3,13 +3,7 @@
 # -*- coding: utf_8 -*-
 
 from django.db import models
-from django.forms import ModelForm
-from django import forms
-import bleach
 
-###################
-# Database Models
-###################
 
 class Clip(models.Model):
     '''Recorded clip elements.'''
@@ -57,59 +51,3 @@ class Tags(models.Model):
     def __unicode__(self):
         '''Return tag when unicode is requested.'''
         return u"%s" % self.tagtext
-    
-#####################
-# Form from Models
-#####################
-
-class ClipForm(ModelForm):
-    '''Form for adding new clips based on model.'''
-    class Meta:
-        '''Build on clip model to create the form.'''
-        model = Clip
-        fields = ('filebits', 'date', 'location', 'tags', 'description')
-
-    taglist = forms.CharField(required=False, help_text='Insert a comma delimited list of descriptive terms for your item.')
-
-    def bleachData(self, data, whitelist=[]):
-        '''Clean data to ensure we have safe HTML to display.'''
-        allowed = whitelist
-        clean_data = bleach.clean(data, allowed)
-    
-        return clean_data	
-
-    def fix_tags(self):
-        '''Transform the taglist input into the tags array.'''
-        pretags = self.cleaned_data['taglist']
-        tagstring = bleach.clean(pretags)
-        tags = []
-
-        for tag in tagstring.split(","):
-            tags.append(Tags.objects.get_or_create(tagtext=tag)[0].pk)
-
-        return tags
-
-    def clean_description(self):
-        '''Do some custom cleaning/validation on the description.'''
-        description = self.cleaned_data['description']
-        whitelist = ['b', 'i']
-        description = self.bleachData(description, whitelist)
-
-        return description
-
-    def clean_location(self):
-        '''Clean the location.'''
-        location = self.cleaned_data['location']
-        locaiton = self.bleachData(location)		
-
-        return location
-
-
-    def clean(self):
-        '''Custom cleaning to override specific fields.'''
-        super(PostForm, self).clean()
-        self.cleaned_data['tags'] = self.fix_tags()
-        
-        # --> Hmm, may need to explicity run the description and location clean
-
-        return self.cleaned_data
